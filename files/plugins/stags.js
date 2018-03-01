90d87f6719c4b29ff5ce75e8d7b321955010006d
Romul81 <romul81@gmail.com>
2018-03-01 21:36:25 +0000

    Completely rewritten
    From 3 to 5 times faster now

--- files/plugins/stags.js
+++ files/plugins/stags.js
@@ -1,342 +1,150 @@
 //Сортирует DSL теги.
 
-function trim(s)
-{
-
-	s = s.replace(/\[(c) +[a-z]+\]/, "$1");
-	s = s.replace(/\[lang +id=\d+?\]/, "lang");
-	s = s.replace(/\[lang +name=[^\[\]]+?\]/, "lang");
-	s = s.replace(/\[\/?(.+)\]/i, "$1");
-
-	return s;
-
+function trimOpeningTag (str) {
+  return str.replace(cTagRe, 'c').replace(langTagRe, 'lang')
 }
 
-
-function sortFunction1(a, b)
-{
-	return (a[0] > b[0]) ? -1 : 1;
+function isChanged (checkArr, refArr) {
+  for (let i = 0; i < checkArr.length; i++) {
+    if (checkArr[i][4] !== refArr[i][4]) return true
+  }
+  return false
 }
 
-
-function sortFunction2(a, b)
-{
-	return (b[0] > a[0]) ? -1 : 1;
+function replaceSlicesArr (str, rangesArr) {
+  if (rangesArr.length > 0) {
+    const tails = str.slice(rangesArr[rangesArr.length - 1][1])
+    str = rangesArr.reduce((acc, val, i, arr) => {
+      const beginning = i === 0 ? 0 : arr[i - 1][1]
+      const ending = arr[i][0]
+      return acc + str.slice(beginning, ending) + arr[i][2]
+    }, '')
+    str += tails
+  }
+  return str
 }
 
+/*******************************************************************/
+const cTagRe = /^\[c [a-z]+\]/
+const langTagRe = /^\[lang [^\]]+\]/
+const allTagRe = /(\\*)(\[(?:\/?(com|[*'bcipu]|ex|lang|trn1?|!trs|sub|sup)|(?:c [a-z]{3,50}|lang (?:id=\d{1,5}|name="\w{5,22}")))\])/g
+const allTagMapArr = []
+let prevIdx = -1
+let count = 0
+let tagMatchArr
 
+while ((tagMatchArr = allTagRe.exec(s))) {
+  if (tagMatchArr[1].length % 2 === 1) continue
 
-function sorttags(arr, closedtags)
-{
+  const tag = tagMatchArr[2]
+  const closing = tag.indexOf('/') === 1
+  const adj = prevIdx === allTagRe.lastIndex - tag.length
+  const trimmed = tagMatchArr[3] ? tagMatchArr[3] : trimOpeningTag(tag)
 
+  if (adj) {
+    allTagMapArr[allTagMapArr.length - 1].push([
+      tag,
+      trimmed,
+      closing,
+      allTagRe.lastIndex,
+      count,
+      -1
+    ])
+  } else {
+    allTagMapArr.push([[tag, trimmed, closing, allTagRe.lastIndex, count, -1]])
+  }
 
-	let m = [];
-
-	for (let i = 0; i < arr.length; i++)
-	{
-		let n = arr[i].split(/:/)[0];
-		let t = arr[i].split(/:/)[1];
-
-		let ctn = 0;
-		for (let j = 0; j < tags.length; j++)
-		{
-
-
-			if ((tags[j][0] > n) && (trim(tags[j][1]) === trim(t)))
-			{
-				ctn = tags[j][0];
-				break;
-			}
-
-		}
-
-		m.push([ctn, t]);
-
-	}
-
-	if (closedtags)
-	{
-		m.sort(sortFunction2);
-	}
-	else
-	{
-		m.sort(sortFunction1);
-	}
-
-	let r = [];
-
-	for (let l = 0; l < m.length; l++)
-	{
-		r.push(m[l][1]);
-	}
-
-	return r;
-
+  count++
+  prevIdx = allTagRe.lastIndex
 }
 
+const allTagArr = new Array(count)
+const allAdjArr = []
+let allAdjCount = 0
+count = 0
 
+// get an array of all adjacent tags
+allTagMapArr.forEach(val => {
+  if (val.length === 1) allTagArr[count++] = val[0]
+  else {
+    val.forEach(tag => {
+      allTagArr[count++] = tag
+      if (!allAdjArr[allAdjCount]) allAdjArr[allAdjCount] = []
+      allAdjArr[allAdjCount].push(tag)
+    })
+    allAdjCount++
+  }
+})
 
+const openingTagArr = allTagArr.filter(tag => !tag[2])
+const closingTagArr = allTagArr.filter(tag => tag[2])
 
-//|||||||||||||||||||| OPEN TAGS SORT START ||||||||||||||||||||
+// sorting adjacent tags function
+const sortBlock = (adjArr, closing) => {
+  const sortDesc = (a, b) => b[1] - a[1]
+  adjArr.forEach(block => {
+    const targetArr = block.filter(tag => tag[2] === closing)
 
-let count = 0;
+    if (targetArr.length > 1) {
+      const newIdxArr = new Array(block.length)
+      const min = block[0][4]
 
-let tags = [];
+      for (let i = 0; i < block.length; i++) {
+        const tag = block[i]
+        const bool = tag[2] === closing
 
-let t = s;
+        if (bool) {
+          const currTagPos = tag[5] === -1 ? tag[4] : tag[5]
+          let oppTagPos = -1
+          if (closing) {
+            for (let j = openingTagArr.length - 1; j >= 0; j--) {
+              const oTag = openingTagArr[j]
+              oppTagPos = oTag[oTag[5] === -1 ? 4 : 5]
+              if (currTagPos > oppTagPos && tag[1] === oTag[1]) break
+            }
+          } else {
+            for (let j = 0; j < closingTagArr.length; j++) {
+              const cTag = closingTagArr[j]
+              oppTagPos = cTag[4]
+              if (currTagPos < oppTagPos && tag[1] === cTag[1]) break
+            }
+          }
 
-let m;
-while (m = /^[^]*?(\\*)(\[(?:\/?(?:com|[\*'bcipu]|ex|trn1?|!trs|sub|sup)|(?:c [a-z]{3,50}|lang id=[0-9]{1,5}|lang name=\"[a-zA-Z]{5,22}\"|\/lang))\])([^]*)$/.exec(t))
-{
-	
-	t = m[3];
+          newIdxArr[i] = [tag, oppTagPos]
+        } else {
+          newIdxArr[i] = [tag, closing ? 0 : count]
+        }
+      }
 
-	if ((m[1].length % 2) === 1)
-	{
-		continue;
-	}
-
-	count++;
-
-	if (/^\[\/[^\n]*\]$/.test(m[2]))
-	{
-		tags.push([count, m[2]]);
-	}
-
+      newIdxArr.sort(sortDesc)
+      if (isChanged(newIdxArr.map(val => val[0]), block)) {
+        newIdxArr.forEach((arr, idx) => (arr[0][5] = idx + min))
+      }
+    }
+  })
 }
 
-
-
-let arr = [];
-
-count = 0;
-
-while (m = /^([^]*?)(\\*)(\[(?:\/?(?:com|[\*'bcipu]|ex|trn1?|!trs|sub|sup)|(?:c [a-z]{3,50}|lang id=[0-9]{1,5}|lang name=\"[a-zA-Z]{5,22}\"|\/lang))\])([^]*)$/.exec(s))
-{
-
-
-	let m1 = m[1];
-	let m2 = m[2];
-	let m3 = m[3];
-	let m4 = m[4];
-
-	s = m4;
-
-
-	if (m1 !== '')
-	{
-		arr.push([m1]);
-	}
-
-	if (m2 !== '')
-	{
-		arr.push([m2]);
-	}
-
-	if ((m2.length % 2) === 1)
-	{
-		continue;
-	}
-
-
-	if ((arr.length > 0) && (/^\d+:\[[^\/\n][^\n]*\]$/.test(arr[arr.length - 1][0])) && (/^\[[^\/\n][^\n]*\]$/.test(m3)))
-	{
-		count++;
-		arr[arr.length - 1].push(count + ':' + m3);
-	}
-	else
-	{
-
-		if (/^\[[^\/\n][^\n]*\]$/.test(m3))
-		{
-			count++;
-			arr.push([count + ':' + m3]);
-		}
-		else if (/^\[\/[^\n]*\]$/.test(m3))
-		{
-			count++;
-			arr.push([m3]);
-		}
-		else
-		{
-			arr.push([m3]);
-		}
-
-	}
-
-
-}
-
-
-arr.push([s]);
-
-
-
-s = '';
-
-for (let i = 0; i < arr.length; i++)
-{
-
-	let r = arr[i];
-
-
-	if (r.length > 1)
-	{
-		r = sorttags(r, false);
-	}
-	else
-	{
-		let m;
-		if (m = /^\d+:(\[[^\/\n][^\n]*\])$/.exec(r[0]))
-		{
-			r[0] = m[1];
-		}
-
-	}
-
-
-	s += r.join("");
-
-}
-
-//|||||||||||||||||||| OPEN TAGS SORT END ||||||||||||||||||||
-
-//------------------------------------------------------------
-
-//|||||||||||||||||||| CLOSIG TAGS SORT START ||||||||||||||||
-
-
-count = 0;
-
-tags.length = 0;
-
-t = s;
-
-while (m = /^([^]*)(\\*)(\[(?:\/?(?:com|[\*'bcipu]|ex|trn1?|!trs|sub|sup)|(?:c [a-z]{3,50}|lang id=[0-9]{1,5}|lang name=\"[a-zA-Z]{5,22}\"|\/lang))\])[^]*?$/.exec(t))
-{
-
-
-
-	let m1 = m[1];
-	let m2 = m[2];
-	let m3 = m[3];
-	t = m1;
-
-	if ((m2.length % 2) === 1)
-	{
-		continue;
-	}
-	
-
-	count++;
-
-	if (/^\[[^\n\/]*\]$/.test(m3))
-	{
-		tags.push([count, m3]);
-	}
-
-
-}
-
-
-
-arr.length = 0;
-
-count = 0;
-
-while (m = /^([^]*)(\\*)(\[(?:\/?(?:com|[\*'bcipu]|ex|trn1?|!trs|sub|sup)|(?:c [a-z]{3,50}|lang id=[0-9]{1,5}|lang name=\"[a-zA-Z]{5,22}\"|\/lang))\])([^]*?)$/.exec(s))
-{
-
-
-	let m1 = m[1];
-	let m2 = m[2];
-	let m3 = m[3];
-	let m4 = m[4];
-
-	s = m1 + m2;
-
-	if (m4 !== '')
-	{
-		arr.push([m4]);
-	}
-
-	if ((m2.length % 2) === 1)
-	{
-		continue;
-	}
-
-
-	if ((arr.length > 0) && (/^\d+:\[\/[^\n]*\]$/.test(arr[arr.length - 1][0])) && (/^\[\/[^\n]*\]$/.test(m3)))
-	{
-		count++;
-		arr[arr.length - 1].push(count + ':' + m3);
-
-	}
-	else
-	{
-
-		if (/^\[\/[^\n]*\]$/.test(m3))
-		{
-			count++;
-			arr.push([count + ':' + m3]);
-		}
-		else if (/^\[[^\/\n][^\n]*\]$/.test(m3))
-		{
-			count++;
-			arr.push([m3]);
-		}
-		else
-		{
-			arr.push([m3]);
-		}
-
-	}
-
-
-}
-
-
-arr.push([s]);
-
-s = '';
-
-for (let i = 0; i < arr.length; i++)
-{
-
-	let r = arr[i];
-
-
-	if (arr[i].length > 1)
-	{
-
-		arr[i] = sorttags(arr[i], true);
-	}
-	else
-	{
-		let m;
-		if (m = /^\d+:(\[\/[^\n]*\])$/.exec(arr[i][0]))
-		{
-			arr[i][0] = m[1];
-		}
-
-	}
-
-
-
-}
-
-
-arr.reverse(); 
-
-for (let i = 0; i < arr.length; i++)
-{
-
-	let r = arr[i];
-	s += r.join("");
-
-}
-
-
-//|||||||||||||||||||| CLOSIG TAGS SORT END ||||||||||||||||
-
+sortBlock(allAdjArr, false)
+sortBlock(allAdjArr, true)
+
+// get slices to replace
+const slices = allAdjArr.filter(val => val[0][5] !== -1).map(val => {
+  const start = val[0][3] - val[0][0].length
+  const end = val[val.length - 1][3]
+  const ret = val
+    .sort((a, b) => a[5] - b[5])
+    .map(v => v[0])
+    .join('')
+  return [start, end, ret]
+})
+
+s = replaceSlicesArr(s, slices)
+
+/*
+const testArr = allTagArr
+  .map(val => {
+    return [val[0], val[5] === -1 ? val[4] : val[5], val[5]]
+  })
+  .sort((a, b) => a[1] - b[1])
+*/
 
