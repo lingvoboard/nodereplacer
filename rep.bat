d53db149faea1ac7aad5576b0632d49486a2813f
lingvoboard <lingvoboard@users.noreply.github.com>
2018-05-03 12:56:58 +0300

    Update rep.bat

--- rep.bat
+++ rep.bat
@@ -2,6 +2,6 @@
 if "%1" == "-h" (
 	start firefox.exe https://github.com/lingvoboard/nodereplacer/blob/master/help/files/md/index.md 
 ) ELSE (
-	node --harmony_regexp_lookbehind --harmony_regexp_property C:\U\nodereplacer\nodereplacer.js %*
+	node C:\U\nodereplacer\nodereplacer.js %*
 	pause
 )
